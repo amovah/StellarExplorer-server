99f26452c38d333b8823a95a2d23627f1434517d
Ali Movahedi <ali_movahedi@aol.com>
2019-11-27 00:53:18 +0330

    feat: connect mongoose to database

--- src/app.js
+++ src/app.js
@@ -1,5 +1,26 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import routers from './routers';
+
+mongoose.connect(
+  `${process.env.DB_URL || `mongodb://localhost/${process.env.DB_NAME}`}`,
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  },
+);
+mongoose.connection.on('error', (error) => {
+  console.error(`Database connection error ${error}`);
+
+  process.exit(0);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.error('Disconnected from database');
+
+  process.exit(0);
+});
 
 const app = express();
 
